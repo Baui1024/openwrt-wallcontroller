b4eab741a8c03d917f5fd24321726b45a71d5343
thomas.bauer <thomas.bauer@qsc.com>
2025-06-27 00:38:10 +0200

    device tree working!
    Signed-off-by: thomas.bauer <thomas.bauer@qsc.com>

---
+++ upgrade.sh
@@ -0,0 +1,2 @@
+scp bin/targets/ramips/mt76x8/openwrt-ramips-mt76x8-onion_omega2p-squashfs-sysupgrade.bin wallcontroller:/tmp/firmware
+ssh wallcontroller sysupgrade /tmp/firmware
---
+++ upload.sh
@@ -0,0 +1,1 @@
+scp bin/targets/ramips/mt76x8/openwrt-ramips-mt76x8-onion_omega2p-squashfs-sysupgrade.bin wallcontroller:/tmp/firmware
