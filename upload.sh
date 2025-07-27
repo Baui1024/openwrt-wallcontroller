c7b26eba589946043e3bd0b55cd183b2bfa9f367
thomas.bauer <thomas.bauer@qsc.com>
2025-07-27 19:48:30 +0200

    removed SD Stuff from DTS
    Signed-off-by: thomas.bauer <thomas.bauer@qsc.com>

--- upload.sh
+++ upload.sh
@@ -1,1 +1,1 @@
-scp bin/targets/ramips/mt76x8/openwrt-ramips-mt76x8-onion_omega2p-squashfs-sysupgrade.bin wallcontroller:/tmp/firmware
+scp bin/targets/ramips/mt76x8/openwrt-ramips-mt76x8-onion_omega2p-squashfs-sysupgrade.bin root@192.168.177.55:/tmp/firmware
